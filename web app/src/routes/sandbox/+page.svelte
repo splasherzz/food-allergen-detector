1cf0daab83b508df09e8f53a1f3a7cc8ff93ef6f
splasherzz <juliasabado30@gmail.com>
2023-07-09 19:51:32 +0800

    change landing page to title only

--- web app/src/routes/sandbox/+page.svelte
+++ web app/src/routes/sandbox/+page.svelte
@@ -43,75 +43,10 @@
 </script>
 
 <main>
-    <div class="all">
-        <h1 style="margin-bottom: 18px;">
-            Welcome to&nbsp;<span class="ai">ai.llergen</span>
-        </h1>
-        <label for="name" class="prompt"
-            >Want to know if your food contains possible allergens?</label
-        >
-        <label for="name" class="prompt" style="margin-bottom: 5px;"
-            >Enter your food product details here:</label
-        >
-        <label for="name" class="prompt2" style="margin-bottom: 25px;"
-            >(Leave text box blank if none)</label
-        >
-
-        <form on:submit|preventDefault={onSubmit}>
-            <div class="form-group">
-                <label for="product" class="textlabel">Name of Food</label>
-                <input
-                    class="form"
-                    type="text"
-                    bind:value={$formValues.product}
-                    required
-                    placeholder="Required"
-                />
-            </div>
-            <div class="form-group">
-                <label for="ingre" class="textlabel">Main Ingredient</label>
-                <input
-                    class="form"
-                    type="text"
-                    bind:value={$formValues.ingre}
-                    required
-                    placeholder="Required"
-                />
-            </div>
-            <div class="form-group">
-                <label for="sweet" class="textlabel">Sweetener</label>
-                <input
-                    class="form"
-                    type="text"
-                    bind:value={$formValues.sweet}
-                />
-            </div>
-            <div class="form-group">
-                <label for="fat" class="textlabel">Fat/Oil</label>
-                <input class="form" type="text" bind:value={$formValues.fat} />
-            </div>
-            <div class="form-group">
-                <label for="seas" class="textlabel">Seasoning</label>
-                <input class="form" type="text" bind:value={$formValues.seas} />
-            </div>
-            <div class="form-group">
-                <label for="aller" class="textlabel">Allergens</label>
-                <input
-                    class="form"
-                    type="text"
-                    bind:value={$formValues.aller}
-                />
-            </div>
-            <div
-                style="display: flex; align-items: center; justify-content: center;"
-            >
-                <button type="submit" class="go">Go!</button>
-
-                {#if isLoading}
-                    <div class="loader" />
-                {/if}
-            </div>
-        </form>
+    <div class="title">
+        <p class="heading">
+            ai.llergen
+        </p>
     </div>
 </main>
 
@@ -121,118 +56,80 @@
         src: url("/fonts/RestoraExtraLight.otf") format("opentype");
     }
 
-    :global(body) {
-        background: #011f4b;
+    @font-face {
+        font-family: "IntroCdReg";
+        src: url("/fonts/IntroCd-Trial-Rg.otf") format("opentype");
     }
 
-    .all {
+    @font-face {
+        font-family: "Magic";
+        src: url("/fonts/Magic Retro.ttf") format("truetype");
+    }
+
+    @font-face {
+        font-family: "IntroCd";
+        src: url("/fonts/IntroCd-Trial-Rg.otf") format("opentype");
+    }
+
+    @font-face {
+        font-family: "BreakLove";
+        src: url("/fonts/Break_Love_Font[1].ttf") format("truetype");
+    }
+
+    @font-face {
+        font-family: "RB";
+        src: url("/fonts/RetroBoldy-Regular.otf") format("opentype");
+    }
+
+    :global(html, body) {
+        height: 98%;
+        background-color: #d35354;
+    }
+
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+    }
+
+    .title {
+        
+        
+    }
+
+    .container1 {
         display: flex;
         flex-direction: column;
-        box-sizing: border-box;
         justify-content: center;
         align-items: center;
-        height: 93vh;
+        background-color: #db7c7c;
+        padding: 20px;
+        border-radius: 25px;
     }
 
-    h1 {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-family: 'RestoraExtraLight', sans-serif;
-        font-size: 3em;
-        color: aliceblue;
+    .heading {
+        font-family: "RB", sans-serif;
+        font-size: 5em;
+        color: #ebe3d3;
+        letter-spacing: 3px;
     }
 
     .ai {
-        color: #ffa552;
+        color: #c1ece4;
     }
+
     .prompt {
-        font-family: "Montserrat", sans-serif;
+        font-family: "IntroCd", sans-serif;
         font-weight: 200;
         font-size: 22px;
         color: aliceblue;
     }
 
     .prompt2 {
-        font-family: "Montserrat", sans-serif;
+        font-family: "IntroCd", sans-serif;
         font-weight: 200;
         font-size: 16px;
         color: aliceblue;
-    }
-
-    .textlabel {
-        font-family: "Montserrat", sans-serif;
-        font-weight: 300;
-        font-size: 18px;
-        color: aliceblue;
-        margin-right: 10px;
-        width: 160px;
-    }
-
-    .form-group {
-        display: flex;
-        align-items: center;
-        margin-bottom: 12px;
-    }
-
-    .form {
-        flex: 1;
-        padding: 6px;
-        border: 1px solid #007bff;
-        border-radius: 4px;
-        font-size: 16px;
-        color: #333;
-        outline: none;
-    }
-
-    .form:focus {
-        border-color: #0056b3;
-        box-shadow: 0 0 0 2px rgba(0, 123, 255, 0.25);
-    }
-
-    .go {
-        background-color: #007bff;
-        color: #fff;
-        padding: 10px 20px;
-        border: none;
-        border-radius: 4px;
-        font-size: 16px;
-        cursor: pointer;
-        margin-top: 7px;
-        width: 120px;
-    }
-
-    .go:hover {
-        background-color: #0056b3;
-    }
-
-    .go:active {
-        background-color: #003980;
-    }
-
-    .loading-container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-    }
-
-    .loader {
-        border: 7px solid #f3f3f3;
-        border-top: 7px solid #3498db;
-        border-radius: 50%;
-        width: 20px;
-        height: 20px;
-        animation: spin 2s linear infinite;
-        margin-left: 15px;
-    }
-
-    @keyframes spin {
-        0% {
-            transform: rotate(0deg);
-        }
-        100% {
-            transform: rotate(360deg);
-        }
     }
 </style>
